2a4ba3e7f340197e025239ca2bd2b02096819b95
Stefano Hepburn <stefanohepburn@gmail.com>
2021-10-24 19:17:25 -0400

    Set up sequelize

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
 const { POSTGRES_PASSWORD, POSTGRES_USER, POSTGRES_DB } = process.env;
-
-module.exports = {
+const Sequelize = require('sequelize')
+const dbenv = {
   development: {
     username: POSTGRES_USER,
     password: POSTGRES_PASSWORD,
@@ -25,3 +25,20 @@ module.exports = {
     dialect: "postgres",
   },
 };
+
+const pg = dbenv[process.env.NODE_ENV]
+
+const db = new Sequelize(pg.database, pg.username, pg.password, {
+    host: pg.host,
+    dialect: pg.dialect,
+    operatorsAliases: false,
+    pool: {
+        max: 5,
+        min: 0,
+        aquire: 30000,
+        idle: 10000
+    },
+})
+// Test db connection
+db.authenticate().then(() => console.log('Database connected')).catch(err => console.log(err))
+module.exports = db
