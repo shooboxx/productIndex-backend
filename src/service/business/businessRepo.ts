80d21b02b8ee48939d7ac4d18c795291d8d94b74
VonyaC <64337036+VonyaC@users.noreply.github.com>
2021-12-17 22:54:20 -0500

    formatted code

--- src/service/business/businessRepo.ts
+++ src/service/business/businessRepo.ts
@@ -1,35 +1,45 @@
 import { Business } from "./businessType";
-const db = require("../../../config/database.js")
+const db = require("../../../config/database.js");
 const Businesses = require("../../models/business");
-const businesses: Business[] = []
+const businesses: Business[] = [];
 
 const findUserBusinesses = async (userId) => {
-  const list: any = []
-  const businesses = await Businesses.findAll({ where: { created_by: userId } })
+  const list: any = [];
+  const businesses = await Businesses.findAll({
+    where: { created_by: userId },
+  });
   if (!businesses) {
-    return null
+    return null;
   }
   for (let i = 0; i < businesses.length; i++) {
-    list.push(businesses[i].dataValues)
+    list.push(businesses[i].dataValues);
   }
-  return list
-
-}
+  return list;
+};
 
 const findBusinessById = async (businessId: number) => {
-  const business = await Businesses.findByPk(businessId)
+  const business = await Businesses.findByPk(businessId);
   if (!businesses) {
-    return null
+    return null;
   }
-  return business.dataValues
-}
+  return business.dataValues;
+};
 
 const createBusiness = async (newBusiness: Business) => {
-  const biz = newBusiness.name.toUpperCase().replace(' ', '')
-  const businesses = await Businesses.findAll({ where: { business_name: db.where(db.fn('UPPER', db.fn('REPLACE', db.col('business_name'), ' ', '')), '=', biz) }, raw: true })
+  const biz = newBusiness.name.toUpperCase().replace(" ", "");
+  const businesses = await Businesses.findAll({
+    where: {
+      business_name: db.where(
+        db.fn("UPPER", db.fn("REPLACE", db.col("business_name"), " ", "")),
+        "=",
+        biz
+      ),
+    },
+    raw: true,
+  });
 
   if (businesses) {
-    return
+    return;
   }
 
   await Businesses.create({
@@ -40,64 +50,72 @@ const createBusiness = async (newBusiness: Business) => {
     email_address: newBusiness,
     created_by: newBusiness.created_by,
     insert_date: Date.now(),
+  }).catch((err) => console.log(err));
 
-  }).catch(err => console.log(err))
-
-  return newBusiness
-
-}
+  return newBusiness;
+};
 
 const removeBusiness = async (businessId) => {
-  const business = await Businesses.findByPk(businessId)
+  const business = await Businesses.findByPk(businessId);
   if (!review) {
-    return
+    return;
   }
-  review.destroy()
-  return review
-
-}
+  review.destroy();
+  return review;
+};
 
 const setBusinessActiveStatus = async (businessId, status) => {
-
-  const business = await Businesses.findByPk(businessId)
+  const business = await Businesses.findByPk(businessId);
 
   if (!business) {
-    return
+    return;
   }
   business.update({
     active: status,
     update_date: Date.now(),
-  })
+  });
 
   return business;
-}
+};
 const updateBusiness = async (businessId, updatedBusiness: Business) => {
-  await Businesses.update({
-    business_name: updatedBusiness.name,
-    description: updatedBusiness.description,
-    category: updatedBusiness.category,
-    profile_picture_url: updatedBusiness.profile_picture_url,
-    active: updatedBusiness.active,
-    update_date: Date.now(),
-  }, {
-    where: {
-      id: businessId
+  await Businesses.update(
+    {
+      business_name: updatedBusiness.name,
+      description: updatedBusiness.description,
+      category: updatedBusiness.category,
+      profile_picture_url: updatedBusiness.profile_picture_url,
+      active: updatedBusiness.active,
+      update_date: Date.now(),
+    },
+    {
+      where: {
+        id: businessId,
+      },
     }
-  })
+  );
   return updatedBusiness;
-}
+};
 
-const findBusinessMasterDetail = () => {
-
-}
+const findBusinessMasterDetail = () => { };
 
 const businessNameMatch = async (bname: string) => {
   //TODO: Figure out how to name match (trim and lowercase)
-  const businesses = await Businesses.findAll({ where: db.fn('upper', db.col('business_name'), bname), raw: true })
+  const businesses = await Businesses.findAll({
+    where: db.fn("upper", db.col("business_name"), bname),
+    raw: true,
+  });
   if (!businesses) {
-    return null
+    return null;
   }
-  return businesses.dataValues
-}
+  return businesses.dataValues;
+};
 
-module.exports = { findBusinessById, findUserBusinesses, createBusiness, removeBusiness, setBusinessActiveStatus, updateBusiness, businessNameMatch }
+module.exports = {
+  findBusinessById,
+  findUserBusinesses,
+  createBusiness,
+  removeBusiness,
+  setBusinessActiveStatus,
+  updateBusiness,
+  businessNameMatch,
+};
