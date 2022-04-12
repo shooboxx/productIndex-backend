00657b1f89d7ec7839ac4bcad0f9d933526b8e03
VonyaC <64337036+VonyaC@users.noreply.github.com>
2022-04-11 21:02:14 -0400

    updated config files

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 if (process.env.NODE_ENV !== 'production') {
-     require('dotenv').config()
+  require('dotenv').config()
 }
-const { db } = require('../config/database.js')
+import db from './models';
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
 const morgan = require('morgan')
 const AppError = require('./utils/appError.js')
-const express = require('express')
+import express from 'express'
 // Initialize the app
 const app = express();
 const bodyParser = require('body-parser')
@@ -22,18 +22,20 @@ let userAuth = require('./service/auth/user/userAuthentication')
 let business = require('./service/business/businessController')
 let review = require('./service/reviews/reviewController')
 let user = require('./service/user/userController')
+let search = require('./service/search/searchController')
+
 
 // Setup server port
 var port = process.env.PORT || 8080;
 
 const limiter = rateLimit({
-     max: 100,
-     windowMs: 60 * 60 * 1000,
-     message: 'Too many request from this IP address. Please try again in 1 hour'
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP address. Please try again in 1 hour'
 })
 
 if (process.env.NODE_ENV === 'development') {
-     app.use(morgan('dev'));
+  app.use(morgan('dev'));
 }
 
 app.use(express.json())
@@ -41,34 +43,42 @@ app.use(methodOverride('_method'))
 app.use(helmet())
 app.use('/api/auth', limiter)
 
+app.use('/api',business)
 app.use('/api',product);
 app.use('/api',store)
 app.use('/api',userAuth)
-app.use('/api',business)
 app.use('/api',review)
 app.use('/api',user)
+app.use('/api',search)
+
 
 app.use(hpp())
 
 
 // Send message for default URL
 app.get('/', (req: any, res: any) => {
-     res.send('Check was successful');
+  res.send('Check was successful');
 
 })
 app.all('*', (req, res, next) => next(new AppError(`Can't find ${req.originalUrl} on this server.`, 404)))
 
 app.use((err, req, res, next) => {
-     err.statusCode = err.statusCode || 500;
-     err.status = err.status || 'error'
-     res.status(err.statusCode).json({
-          status: err.status,
-          message: err.message
-     })
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error'
+  res.status(err.statusCode).json({
+       status: err.status,
+       message: err.message
+  })
 })
+
+db.sequelize
+.authenticate()
+.then(() => {
+    app.listen(port, function () {
+        console.log("Running RestHub on port " + port);
+      })
+}).catch((err : any) => console.log(err));
+
 // Launch app to listen to specified port
-app.listen(port, function () {
-     console.log("Running RestHub on port " + port);
-});
 
 
