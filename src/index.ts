f8fdaa0a921ed4b127509af87d2293bee57e0617
Stanico Knowles <ashadknowles@gmail.com>
2022-07-17 18:09:46 -0400

    add help desk route to index

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,7 @@ let business = require('./service/business/businessController')
 let review = require('./service/reviews/reviewController')
 let user = require('./service/user/userController')
 let search = require('./service/search/searchController')
+let helpDesk = require('./service/help-desk/helpDeskController')
 
 
 // Setup server port
@@ -60,6 +61,7 @@ app.use('/api',userAuth)
 app.use('/api',review)
 app.use('/api',user)
 app.use('/api',search)
+app.use('/api',helpDesk)
 
 
 app.use(hpp())
