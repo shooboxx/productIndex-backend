4080ee86fb973113e8492458717fb6997a3fe210
Stanico Knowles <ashadknowles@gmail.com>
2022-07-17 15:56:31 -0400

    create errors enum

---
+++ src/service/help-desk/helpDeskErrors.ts
@@ -0,0 +1,6 @@
+export const HelpDeskErrors = {
+    UserIdRequired: "User id is required.",
+    BusinessIdRequired: "Business id is required.",
+    ProductIdRequired: "Product id is required.",
+    MessageRequired: "Message is required.",
+}
--- src/service/help-desk/helpDeskService.ts
+++ src/service/help-desk/helpDeskService.ts
@@ -1,5 +1,6 @@
 import AppError from '../../utils/appError.js'
 import { HelpDeskRepo } from './helpDeskRepo.js'
+import { HelpDeskErrors } from './helpDeskErrors.js'
 
 const createTicket = async (userId: number, businessId: number, productId: number, message: string) => {
     userIdIsPresent(userId)
@@ -10,19 +11,19 @@ const createTicket = async (userId: number, businessId: number, productId: numbe
 }
 
 const userIdIsPresent = (userId: number) => {
-    if (!userId) throw new AppError("User id is required.")
+    if (!userId) throw new AppError(HelpDeskErrors.UserIdRequired)
 }
 
 const businessIdIsPresent = (businessId: number) => {
-    if (!businessId) throw new AppError("Business id is required.")
+    if (!businessId) throw new AppError(HelpDeskErrors.BusinessIdRequired)
 }
 
 const productIdIsPresent = (productId: number) => {
-    if (!productId) throw new AppError("Product id is required.")
+    if (!productId) throw new AppError(HelpDeskErrors.ProductIdRequired)
 }
 
 const messageIsPresent = (message: string) => {
-    if (!message) throw new AppError("Message is required.")
+    if (!message) throw new AppError(HelpDeskErrors.MessageRequired)
 }
 
 export const HelpDeskService = {
