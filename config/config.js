c596c4b8bd8c0d0ef4e34ad1c735aa4efa5c4892
VonyaC <64337036+VonyaC@users.noreply.github.com>
2022-08-08 19:31:58 -0400

    added env variable to db config file

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
     "username": process.env.POSTGRES_USER,
     "password": process.env.POSTGRES_PASSWORD,
     "database": process.env.POSTGRES_DB,
-    "host": "localhost",
+    "host": process.env.DB_HOST,
     "dialect": "postgres"
   },
   "test": {
