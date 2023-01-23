f59b8c9be271a4428f6f57215ab3b7d4a3f475ab
Stefano Hepburn <stefanohepburn@gmail.com>
2023-01-23 11:59:08 -0500

    Completed TODOs

--- src/service/store/businessStoreRepo.ts
+++ src/service/store/businessStoreRepo.ts
@@ -7,7 +7,6 @@ import db from "../../models";
 const findStore = async (storeId : number, storeName : string ) : Promise<BusinessStore> => {
   return await db.BusinessStore.findOne({
       where: {
-          //TODO: Add a like and compare by case
           [Op.or]: [{id: storeId, deleted_date: null}, {unique_name: storeName, deleted_date: null}]
       },
       include: [{model: db.StoreContacts, attributes: {exclude: ['insert_date', 'update_date', 'id']}}, {model: db.StoreHours, attributes: {exclude: ['id', 'business_store_id','insert_date', 'update_date']}}, {model: db.Business, attributes: {exclude: ['insert_date', 'update_date', 'deleted_date']}}],
